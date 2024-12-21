00a985b507c442b216f36a463fe80d437c4bc322
shishir-shukla19 <shishir.shukla@accenture.com>
2024-12-21 10:29:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the fs module to read the file and send it to the client.
+// Add a console.log to confirm the server is running.
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./comments.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
